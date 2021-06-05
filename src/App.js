bdbdb4cee7527a20009a0b73456ffafe03e84db6
shubh <shubhnaik.19@gmail.com>
2021-06-05 18:12:30 +0530

    Generation of otp using Mobile number

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
 import './App.css';
+import React from 'react';
+import {Component} from 'react';
+import FormComponent from './Component/formComponent'
+class App extends Component{
+  constructor(){
+    super();
+    this.state ={
+      txnId : "",
+    };
+  }
 
+<<<<<<< Updated upstream
 function App() {
   return (
     <div className="App">
@@ -7,5 +18,29 @@ function App() {
     </div>
   );
 }
+=======
+  getOtp = async (e) =>{
+    e.preventDefault();
+    const mobileNumber = e.target.elements.mobNo.value;
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ "mobile":`${mobileNumber}`})
+  };
+  const api_call = await fetch('https://cdn-api.co-vin.in/api/v2/auth/public/generateOTP', requestOptions);
+  const response = await api_call.json();
+  this.setState({txnId:response.txnId});
+  console.log(response, this.state.txnId);
+  }
+  
+  render(){
+    return (
+      <div className="App">
+      <FormComponent getOtp={this.getOtp}></FormComponent>      
+      </div>
+    );
+  };
+ }
+>>>>>>> Stashed changes
 
 export default App;
---
+++ src/Component/formComponent.js
@@ -0,0 +1,18 @@
+import {Component} from 'react';
+
+class formComponent extends Component {
+    state = {  }
+    render() {
+        return (
+            <div className="container">
+        <form onSubmit={this.props.getOtp}>
+        <div className="otpForm"><input type="text" className="otpForm" name="mobNo" placeholder="Mobile No." autoComplete="off"/></div>
+        <div className="button submit"><button className="otpButton">Generate OTP</button></div>
+        </form>
+        </div>
+            
+        );
+    }
+}
+
+export default formComponent;
