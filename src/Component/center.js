22b26169c6506ec400633dd7b1572553690cd61a
shubh <shubhnaik.19@gmail.com>
2021-06-06 18:55:23 +0530

    Added card for center name and dose available

---
+++ src/Component/center.js
@@ -0,0 +1,22 @@
+import { Component } from 'react';
+
+
+class center extends Component {
+    state = {  }
+    render() {
+        return (
+            <div className={centerStyle}>
+                <h1>Center Name : {this.props.center.name}</h1>
+                <h3>Dose Available :{this.props.center.available_capacity}</h3>
+                {console.log(this.props.center.name)}
+            </div>
+        );
+    }
+}
+const centerStyle = {
+    display: 'grid',
+    gridTemplateColumns: 'repeat(3, 1fr)',
+    gridGap: '1rem'
+  }
+
+export default center;
